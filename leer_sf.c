dcaccb6448595eaddc9ebce7e4a8c010d53660f9
RubenLopBon <56294473+RubenLopBon@users.noreply.github.com>
2020-03-12 16:25:50 +0100

    Add files via upload

---
+++ leer_sf.c
@@ -0,0 +1,23 @@
+#include "ficheros_basico.h"
+
+int main(int argc, char **argv){
+	bmount(argv[1]);
+	struct superbloque SB;
+	bread(posSB, &SB);
+
+	printf("DATOS DEL SUPERBLOQUE:\n");
+    printf("posPrimerBloqueMB = %u\n", SB.posPrimerBloqueMB);
+    printf("posUltimoBloqueMB = %u\n", SB.posUltimoBloqueMB);
+    printf("posPrimerBloqueAI = %d\n", SB.posPrimerBloqueAI);
+    printf("posUltimoBloqueAI = %d\n", SB.posUltimoBloqueAI);
+    printf("posPrimerBloqueDatos = %d\n", SB.posPrimerBloqueDatos);
+    printf("posUltimoBloqueDatos = %d\n", SB.posUltimoBloqueDatos);
+    printf("posInodoRaiz = %d\n", SB.posInodoRaiz);
+    printf("posPrimerInodoLibre = %d\n", SB.posPrimerInodoLibre);
+    printf("cantBloquesLibres = %d\n", SB.cantBloquesLibres);
+    printf("canInodosLibres = %d\n", SB.cantInodosLibres);
+    printf("totBloques = %d\n", SB.totBloques);
+    printf("totInodos = %d\n\n", SB.totInodos);
+	
+	bumount();
+}  
