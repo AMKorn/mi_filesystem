c2b5c8a92c683886ddad869a1bdeb95302af3ee7
RubenLopBon <56294473+RubenLopBon@users.noreply.github.com>
2020-06-07 17:03:35 +0200

    VERSION COPYPASTE
    NO UTILIZAR EN LA VERSION FINAL SIN MODIFICAR

--- verificacion.c
+++ verificacion.c
@@ -1,102 +1,133 @@
 #include "verificacion.h"
+#define NOMBRE_FICHERO_PRUEBA "prueba.dat"
+#define NUM_PROCESOS NUMPROCESOS
 
-#define DISCO   argv[1]
-#define DIR     argv[2]
+int main (int argc, char **argv) {
+    // Comprobamos los parametros
+	if (argc != 3) {
+		printf("ERROR: faltan argumentos\n");
+		printf("SYNTAX:\n $./verificacion <disco> </directorio> \n");
+		exit(0);
+	}
+	
+	// Montamos el dispositivo virtual
+	if (bmount(argv[1]) == -1) {
+		printf("ERROR: Main: Fallo al montar el sistema de archivos\n");
+		exit(0);
+	}
+	
+	// Obtenemos la metainformacion del directorio de simulacion
+	struct STAT stat;
+ 	if (mi_stat(argv[2], &stat) == -1) {
+		printf("ERROR: main: Fallo obteniendo la metainformacion del directorio de simulacion\n");
+		exit(0);
+	}
 
-int main(int argc, char **argv){
-    // Comprobar sintaxis
-    if(argc != 3){
-        fprintf(stderr, "Sintaxis incorrecta: ./verificacion <disco> <directorio_simulacion>\n");
-        return EXIT_FAILURE;
+	printf("\ndir_sim: %s\n", argv[2]);
+    
+    int numentradas = stat.tamEnBytesLog / sizeof(struct entrada);
+    if (numentradas != NUM_PROCESOS) {
+        return -1;
     }
+	printf("numentradas: %d NUM_PROCESOS: %d\n", numentradas, NUM_PROCESOS);
 
-    //Montamos el disco
-    if(bmount(DISCO)==-1) {
-        fprintf(stderr, "Error de montaje de disco.\n");
-        return EXIT_FAILURE;
-    }
+    // Inicializamos el camino del fichero de informe
+	char camino_fichero_informe[128];
+	sprintf(camino_fichero_informe, "%s%s", argv[2], "informe.txt");
+	// Creamos el fichero de informe
+	if (mi_creat(camino_fichero_informe, 7) == -1) {
+		printf("ERROR: main: Error creando el fichero informe\n");
+		exit(0);
+	}
+	
+	struct entrada entradas[NUM_PROCESOS];
+	if (mi_read(argv[2], entradas, 0, sizeof(entradas)) == -1) {
+		printf("ERROR: main: Error leyendo entradas del directorio de simulacion\n");
+		exit(0);
+	}
+	int i, nbytes_informe = 0;
+	for (int i = 0; i < NUM_PROCESOS; i++) {
+	    struct INFORMACION info;
+	    info.pid = atoi(strchr(entradas[i].nombre, '_') + 1);
+		
+		info.nEscrituras = 0;
+		char camino_fichero_prueba[128];
+		sprintf(camino_fichero_prueba, "%s%s/%s", argv[2], entradas[i].nombre, NOMBRE_FICHERO_PRUEBA);
+		
+		int offset = 0;
+		
+		int cant_registros_buffer_escrituras = 6144; //
+		struct REGISTRO buffer_escrituras [cant_registros_buffer_escrituras];
+		while (mi_read(camino_fichero_prueba,buffer_escrituras,offset, sizeof(buffer_escrituras)) > 0) {
+			int j;
+            for (j = 0; j < cant_registros_buffer_escrituras; j++) {
+				if (buffer_escrituras[j].pid == info.pid) {
+					
+					if (info.nEscrituras == 0) {
+						info.PrimeraEscritura = buffer_escrituras[j];
+						info.UltimaEscritura = buffer_escrituras[j];
+						info.MenorPosicion = buffer_escrituras[j];
+						info.MayorPosicion = buffer_escrituras[j];
+					} else {
+						if(info.PrimeraEscritura.nEscritura > buffer_escrituras[j].nEscritura){
+							info.PrimeraEscritura = buffer_escrituras[j];
+						} else if(info.UltimaEscritura .nEscritura < buffer_escrituras[j].nEscritura){
+							info.UltimaEscritura  = buffer_escrituras[j];
+						}
+						if (buffer_escrituras[j].nEscritura < info.MenorPosicion.nEscritura) {
+							info.MenorPosicion = buffer_escrituras[j];
+						} else if (buffer_escrituras[j].nEscritura > info.MayorPosicion.nEscritura) {
+							info.MayorPosicion = buffer_escrituras[j];
+						}
+					}
+					info.nEscrituras++;
+				}
+			}
+			memset(buffer_escrituras, 0, sizeof(buffer_escrituras));
+			offset += sizeof(buffer_escrituras);
+        }
+        // Escribimos la informacion del registros [j]_info en el fichero de informe
+		char buffer[1024];
+		memset(buffer, 0, 1024);
+		sprintf(buffer, "PID: %i\nNumero de escrituras: %i\n", info.pid, info.nEscrituras);
 
-    // Accedemos a los stats del directorio de simulacion 
-    struct STAT stats;
-    if(mi_stat(DIR, &stats) == EXIT_FAILURE) {
-        fprintf(stderr, "Error al acceder a los stats del directorio de simulación.\n");
-        return EXIT_FAILURE;
-    }
+		sprintf(buffer + strlen(buffer), "%s %i %i %s",
+		"Primera escritura: ",
+		info.PrimeraEscritura.nEscritura,
+		info.PrimeraEscritura.nEscritura,
+		asctime(localtime(&info.PrimeraEscritura.fecha)));
 
-    int numentradas = stats.tamEnBytesLog / sizeof(struct entrada);
-    if(numentradas != NUMPROCESOS) {
-        fprintf(stderr, "Error: El número de entradas no coincide con el número de procesos.\n");
-        return EXIT_FAILURE;
-    }
+		sprintf(buffer + strlen(buffer), "%s %i %i %s",
+		"Ultima escritura: ",
+		info.UltimaEscritura.nEscritura,
+		info.UltimaEscritura.nEscritura,
+		asctime(localtime(&info.UltimaEscritura.fecha)));
 
-    printf("Número de entradas: %d\n", numentradas);
+		sprintf(buffer + strlen(buffer), "%s %i %i %s",
+		"Menor posicion: ",
+		info.MenorPosicion.nEscritura,
+		info.MenorPosicion.nEscritura,
+		asctime(localtime(&info.MenorPosicion.fecha)));
 
-    // Creamos el fichero informe.txt dentro del directorio de simulación
-    char camino_informe[128] = "";
-    strcpy(camino_informe, DIR);
-    strcat(camino_informe, "informe.txt");
+		sprintf(buffer + strlen(buffer), "%s %i %i %s %s",
+		"Mayor posicion: ",
+		info.MayorPosicion.nEscritura,
+		info.MayorPosicion.nEscritura,
+		asctime(localtime(&info.MayorPosicion.fecha)),
+		"\n");
 
-    if(mi_creat(camino_informe, 7) == -1){
-        fprintf(stderr, "Error creando el fichero informe.txt\n");
-        return EXIT_FAILURE;
-    }
-
-    struct entrada ent;
-    //Para cada entrada del directorio de simulación (es decir, para cada proceso) hacer    
-    for(int i = 0; i < NUMPROCESOS; i++){  
-        // Leer la entrada de directorio.
-        if (mi_read(DIR, &ent, 0, sizeof(ent)) == -1) {
-            printf("Error: En la lectura del directorio.\n");
-            return EXIT_FAILURE;
-	    } 
-
-        struct INFORMACION info;
-        //Extraer el PID a partir del nombre de la entrada y guardarlo en el registro info.
-        info.pid= atoi(strchr(ent.nombre, '_'));
-        info.nEscrituras = 0;
-        char camino_prueba[128] = "";
-        strcpy(camino_prueba, DIR);
-        strcat(camino_prueba, ent.nombre);
-        strcat(camino_prueba, "prueba.dat");
-        
-        int cant_registros_buffer_escrituras = 256;
-        struct REGISTRO buffer_escrituras[cant_registros_buffer_escrituras];
-
-        struct REGISTRO reg;
-        /* Recorrer secuencialmente el fichero prueba.dat utilizando un buffer de N registros de escrituras: */
-        // while(mi_read(camino_prueba, buffer_escrituras, 0, sizeof(buffer_escrituras)) > 0){ //mientras haya escrituras
-        //     Leer una escritura => _escritura_
-        //     if(_escritura_.pid == info.pid){
-        //         if(info.nEscrituras=0){
-        //             info.PrimeraEscritura = _escritura_.PrimeraEscritura;
-        //             info.UltimaEscritura = _escritura_.UltimaEscritura;
-        //             info.MenorPosicion = _escritura_.MenorPosicion;
-        //             info.MayorPosicion = _escritura_.MayorPosicion;
-        //         } else {
-        //             // Comparar fecha  y, para una misma fecha, el nº de escritura (para obtener la primera y la última escritura)
-        //             // con los datos de nuestros registros y actualizar éstos si es necesario.
-        //             int dif = difftime(info.UltimaEscritura.fecha, _escritura_.UltimaEscritura.fecha)==0);
-        //             if(dif==0){
-        //                 dif = info.UltimaEscritura.nEscritura - _escritura_.UltimaEscritura.nEscritura;
-        //             }
-        //             if(dif<0) { // Nos quedamos el mayor, el mas reciente
-        //                 // Si info - _escritura_ < 0, _escritura > info
-        //                 info.PrimeraEscritura = _escritura_.PrimeraEscritura;
-        //                 info.UltimaEscritura = _escritura_.UltimaEscritura;
-        //                 info.MenorPosicion = _escritura_.MenorPosicion;
-        //                 info.MayorPosicion = _escritura_.MayorPosicion;
-        //             }
-        //         }
-        //         info.nEscrituras++;
-        //     }
-        // }
-        // Obtener la escritura de la última posición //será la delimitada por el EOF
-        // Añadir la información del struct info al fichero informe.txt por el final 
-    }
-
-    //Desmontamos el disco
-    if(bumount(DISCO)==-1) {
-        fprintf(stderr, "Error al desmontar el disco.\n");
-        return EXIT_FAILURE;
-    }
+		if (mi_write(camino_fichero_informe, buffer, nbytes_informe, strlen(buffer)) == -1) {
+			printf("ERROR: main: Error escribiendo en el informe\n");
+		}
+		nbytes_informe += strlen(buffer);
+	printf("%d escrituras validadas en %s\n", info.nEscrituras, camino_fichero_prueba);
+	}
+	// Desmontamos el dispositivo
+	if (bumount() == -1) {
+		printf("ERROR: main: Error desmontando el dispositivo\n");
+		exit(0);
+	}
+	printf("Verificacion finalizada\n");
+	return 0;
+	
 }
